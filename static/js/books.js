0472705e986faef02a1b9d983ca7577120590bf9
Alex McDonald <qrmf.dev@outlook.com>
2021-04-06 01:26:10 +0100

    🎉 Initial commit

---
+++ static/js/books.js
@@ -0,0 +1,10 @@
+window.books = [
+  {
+    id: 'FFN1840111',
+    title: 'Biology',
+    author: 'Shivani',
+    blurb: 'Slash, HPBZ - It wasn\'t just power and ability that Tom Riddle transferred to Harry that night. He made him the son he never had, and that changes everything.',
+    // INFO: will be updated to a download link once FanFicFare starts to work again
+    downloadLink: 'https://www.fanfiction.net/s/1840111'
+  }
+];
---
+++ static/js/render-content.js
@@ -0,0 +1,72 @@
+// Imports
+const dialog = new mdc.dialog.MDCDialog(document.querySelector('.mdc-dialog'));
+
+// Functions
+const createBookListItem = (bookObject) => {
+  const booklist = document.getElementById('booklist');
+  const listItem = document.createElement('li');
+  const rippleSpan = document.createElement('span');
+  const textSpan = document.createElement('span');
+  const primaryTextSpan = document.createElement('span');
+  const secondaryTextSpan = document.createElement('span');
+
+  //@debug
+  console.info(`DEBUG: Creating and populating a book entry for book ID: ${bookObject.id}`);
+
+  // Root LI Element
+  listItem.classList = 'mdc-list-item';
+  listItem.tabIndex = 0;
+
+  // Ripple Span Element
+  rippleSpan.classList = 'mdc-list-item__ripple';
+
+  // Text Span Element
+  textSpan.classList = 'mdc-list-item__text';
+
+  // Primary Text Span Element
+  primaryTextSpan.classList = 'mdc-list-item__primary-text';
+  primaryTextSpan.innerText = bookObject.title;
+
+  // Secondary Text Span Element
+  secondaryTextSpan.classList = 'mdc-list-item__secondary-text';
+  secondaryTextSpan.innerText = `by ${bookObject.author}${bookObject.platform ? ` on ${bookObject.platform}` : ''}`;
+
+  // Create Hierarchy
+  //@debug
+  console.info(`DEBUG: Creating element hierarchy for book ID: ${bookObject.id}`);
+
+  textSpan.appendChild(primaryTextSpan);
+  textSpan.appendChild(secondaryTextSpan);
+
+  listItem.appendChild(rippleSpan);
+  listItem.appendChild(textSpan);
+
+  //@debug
+  console.info(`DEBUG: Adding book ID: ${bookObject.id} to booklist`);
+
+  booklist.appendChild(listItem);
+
+  // Event handlers
+  listItem.onclick = () => {
+    // Manipulate Dialog content
+    dialog.content.innerText = bookObject.blurb;
+
+    // Edit Click events for Dialog buttons
+    dialog.buttons[0].onclick = () => {
+      //@debug
+      console.info(`DEBUG: Opening book ID: ${bookObject.id} (${bookObject.downloadLink})`);
+      location.assign(bookObject.downloadLink);
+    }
+    dialog.buttons[1].onclick = dialog.close;
+
+    // Open Dialog
+    //@debug
+    console.info(`DEBUG: Open download dialog for book ID: ${bookObject.id}`);
+    dialog.open();
+  }
+}
+
+// Function Calls
+let _books = [...window.books].reverse();
+
+_books.forEach(book => createBookListItem(book));
